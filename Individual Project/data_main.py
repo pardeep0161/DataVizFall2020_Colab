3b7d1303042577cea976b3e1ca35d7deb5b7f7fd
Pardeep Pathania <8322174+pardeep0161@users.noreply.github.com>
2020-10-30 00:06:14 -0400

    Week 9 Eexercise

--- Individual Project/data_main.py
+++ Individual Project/data_main.py
@@ -1,16 +1,17 @@
 import pandas as pd
 
-
-SPI_URL = "C:\\Users\\Parde\\PycharmProjects\\pythonProject\\Individual Project\\data\\SPI.xlsx"
-EFI_URL = "C:\\Users\\Parde\\PycharmProjects\\pythonProject\\Individual Project\\data\\EFI.csv"
+SPI_URL = "D:\\UMassD\\Semester 2 Fall\\CIS 568\\DataVizFall2020_Colab\\Individual Project\\data\\SPI.xlsx"
+EFI_URL = "D:\\UMassD\\Semester 2 Fall\\CIS 568\\DataVizFall2020_Colab\\Individual Project\\data\\EFI.csv"
 
 
 def get_data():
     spi_data = pd.read_excel(SPI_URL)
     res = spi_data[spi_data['SPI year'] == 2020]
-    res = res.sort_values('Social_Progress_Index').head(10)
+    # res = res.sort_values('Social_Progress_Index').head(10)
     res = res[['SPI_Rank', 'Country', 'Social_Progress_Index']]
     res = res.reset_index(drop=True)
-    return res.to_json(orient="records")
+    return res
+    # return res.to_json(orient="records")
 
-# print(get_data())
+
+print(get_data()['Country'])
